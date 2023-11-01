0dcf7d4c87f68b4bef4bea867c57b8a17d83435d
Zoi <zoilanbond@gmail.com>
2023-11-01 14:41:22 -0700

    basic functionality part 2

--- 2-http/http.js
+++ 2-http/http.js
@@ -5,19 +5,35 @@
 
  const server = http.createServer((req, res) => {
      var parsedUrl = url.parse(req.url, true)
-     var index = parsedUrl.path.split('/')[2]
+     var urlArray = parsedUrl.path.split('/')
      
 
     var pets;
-    if (req.method === 'GET' && req.url === '/pets') {
+    if (req.method === 'GET' && urlArray[1] === 'pets') {
         fs.readFile('../pets.json', 'utf8', (error, data) => {
-            pets = data
-            console.log(pets)
-            res.statusCode = 200
-            res.setHeader('Content-Type', 'application-json');
-            res.end(pets)
+            var parsedData = JSON.parse(data);
+            var index = urlArray[2];
+
+            if (!urlArray[2]) {
+                pets = data
+                console.log(pets)
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'application-json');
+                res.end(pets)
+            } else if (!parsedData[index]) {
+                errorMessage()
+            } else {
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'application-json');
+                res.end(JSON.stringify(parsedData[index])); 
+            }
         })
     } else {
+        errorMessage()
+    }
+
+
+    function errorMessage() {
         res.statusCode = 404
         res.setHeader('Content-Type', 'text/plain');
         res.end('Not found');
@@ -27,3 +43,5 @@
  server.listen(port, () => {
     console.log('Listening on port', port)
  })
+
+ 
