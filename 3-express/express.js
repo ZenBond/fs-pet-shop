01686a5c18f8424707e070bf4526589e30ea19dc
Zoi <zoilanbond@gmail.com>
2023-11-02 16:11:01 -0700

    clean up

--- 3-express/express.js
+++ 3-express/express.js
@@ -50,57 +50,6 @@ app.use((err, req, res, next) => {
 app.listen(port, () => {
     console.log('Listening on port:', port)
  })
-// const server = http.createServer((req, res) => {
-
-    
-
-//    var pets;
-//    if (req.method === 'GET' && urlArray[1] === 'pets') {
-       
-//    } else if (req.method === 'POST' && urlArray[1] === 'pets'){
-//        let data = ''
-//        req.on('data', (chunk) => {
-//            data += chunk
-//            console.log(data)
-//        })
-
-//        req.on('end', () => {
-//            const parseData = JSON.parse(data);
-//            console.log(parseData)
-//        })
-       
-//        fs.readFile('../pets.json', 'utf8', (error, data) => {
-//            const newData = JSON.parse(data)
-//            newData.push(data)
-//            fs.writeFile('../pets.json', 'utf8', (error, data) => {
-
-//            })
-//        })
-
-
-
-
-       
-       
-//    } else {
-//        errorMessage()
-//    }
-
-   
-
-
-//    function errorMessage() {
-//        res.statusCode = 404
-//        res.setHeader('Content-Type', 'text/plain');
-//        res.end('Not found');
-//    }
-
-//    function successMessage(result) {
-//        res.statusCode = 200
-//        res.setHeader('Content-Type', 'application-json');
-//        res.end(result); 
-//    }
-// })
 
 
 
